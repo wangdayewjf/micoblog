fcfb177b17925c9b9e92a84b1704cfa4760e5f62
wb-wjf284713 <wb-wjf284713@alibaba-inc.com>
2017-06-20 11:28:22 +0800

    页面调试成功

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = require('./routes');
 var app = express();
 var path = require('path');
+var flash = require("connect-flash");
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var settings = require("./settings");
@@ -9,10 +10,11 @@ var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var cookieSession = require('cookie-session');
-partials = require('express-partials');
+var partials = require('express-partials');
+
 
 app.use(partials());
- app.use(flash());
+
 app.set("views", __dirname + "/views");
 app.set('view options', {
 layout: true
@@ -21,29 +23,37 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride())
 app.use(cookieParser());
+
+
+//app.use(express.router(routes));
 app.use(session({
 	secret: settings.cookieSecret,
+	key:settings.db,                        //cookie的名字  
+    cookie:{maxAge:1000*60*60*24*30},  
 	store: new MongoStore({
 		host: ' 127.0.0.1',
         port: '27017',
 		db: settings.db,
 		url: settings.url
-		})
+		}),
+	resave:true,  
+    saveUninitialized:false
 }));
-//app.use(express.router(routes));
+app.use(flash());
 
-app.user(function(req,res,next){
-	res.local.user = req.session.user;//问：这个会直接到mongodb数据库里面获取么？
-	var err = req.flush("error");
-	var success = req.flush("success");
-	res.local.error = err.length?err:null;
-	res.local.sucess = success.length?sucess:null;
+app.use(function(req,res,next){
+	res.locals.user = req.session.user;//问：这个会直接到mongodb数据库里面获取么？
+	var err = req.flash("error");
+	var success = req.flash("success");//问：这里面如果还没有值怎么办？回自动生成一个key?
+	res.locals.error = err.length?err:null;
+	res.locals.success = success.length?sucess:null;
 
 	next();//执行下一个
-});
+});//拦截器的作用。debug一下看谁先执行，谁后执行。
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(3001);
 
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get("/reg", function(req, res) {
 });
 router.post("/reg",function(req,res){
 	if(req.body["password-repeat"]!=req.body["password"]){
-		req.flush("error","俩次输入的口令不一致");//这里哦的flush我暂且理解成，不一致，等会，安装一下flush
+		req.flash("error","俩次输入的口令不一致");//这里哦的flush我暂且理解成，不一致，等会，安装一下flush
 		return res.redirect("/reg");//问：重定向是默认的是Get吧？
 
 	}
@@ -34,16 +34,16 @@ router.post("/reg",function(req,res){
 			err = "username already exist";//err在哪定义的？答：是个参数吧？
 		}
 		if(err){
-			req.flush("error",err);
+			req.flash("error",err);
 			return res.redirect("/reg");
 		}
 		newUser.save(function(err,inUser){
 			if(err){
-				req.flush("error",err);
+				req.flash("error",err);
 				return res.redirect("/reg");
 			}
 			req.session.user = newUser ;//问：存在session里面？不是存在mongdb里面的么？问：这里的session知识一个形象其实还是req?,换句话说就是response里面的一个字段？问：这样会显示在页面中，还是为了保存user？也就是说保存到mogodb中？或者是俩者都有？
-			req.flush("success","注册成功");
+			req.flash("success","注册成功");
 			res.redirect("/");
 		});
 	});
